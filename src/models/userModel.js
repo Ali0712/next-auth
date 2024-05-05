3ce0417d371391edcaa5d3c2b5200c7f12ee9f8a
Muhammad Ali <muhammadali07122@gmail.com>
2024-05-05 14:05:36 +0500

    feat: Add user model with required fields and default values

---
+++ src/models/userModel.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Please provide a username"],
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide an email"],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Please provide a password"],
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+    isVerified: {
+        type: Boolean,
+        default: false,
+    },
+    forgotPasswordToken: String,
+    forgotPasswordExpiry: Date,
+    verifyToken: String,
+    verifyTokenExpiry: Date
+})
+
+const User = mongoose.model("User", userSchema);
