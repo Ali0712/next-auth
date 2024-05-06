d33f9dd09dfcbb3d96fc83d4ba1ea1a26567ed3f
Muhammad Ali <muhammadali07122@gmail.com>
2024-05-06 12:01:52 +0500

    changes

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,41 @@
+import { connect } from "@/dbConfig/dbConfig";
+import bcryptjs from "bcryptjs";
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/models/userModel";
+import jwt from "jsonwebtoken";
+
+connect();
+
+export async function POST(request: NextRequest) {
+    try {
+        const reqBody = await request.json();
+        const {email, password} = reqBody;
+        console.log(reqBody);
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return NextResponse.json({message: "User does not exist"},{status: 400})
+        }
+
+        const validPassword = await bcryptjs.compare(password, user.password)
+        if (!validPassword) {
+            return NextResponse.json({message: "Invalid password"},{status: 400})
+        }
+
+        const tokenPayload = {id: user._id, email: user.email};
+        const token = jwt.sign(tokenPayload, process.env.TOKEN_SECRET!, {expiresIn: "1d"});
+
+        const response = NextResponse.json({
+            message: "User logged in successfully",
+            success: true
+        }, { status: 200})
+
+        response.cookies.set("token", token, {
+            httpOnly: true,
+        })
+        return response;
+        
+    } catch (error: any) {
+        return NextResponse.json({ message: error.message }, { status: 500 });
+    }
+}
